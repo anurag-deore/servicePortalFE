7f6c46b819174761b37bd98df19a61adb9165c54
Anurag Ravindra Deore <anuragdeore125@gmail.com>
2022-07-22 09:23:15 +0000

    New style added

--- src/App.js
+++ src/App.js
@@ -1,70 +1,139 @@
-import React, { useState, useEffect } from 'react';
-import styled, { ThemeProvider } from "styled-components";
-import WebFont from 'webfontloader';
-import { GlobalStyles } from './theme/GlobalStyles';
-import {useTheme} from './theme/useTheme';
-
-import ThemeSelector from './ThemeSelector';
-
-import Dialog from './Dialog';
-import CreateThemeContent from './CreateThemeConten';
-
-const Container = styled.div`
-  margin: 5px auto 5px auto;
-`;
-
+import React, { useEffect, useState } from "react";
+import { GlobalStyles } from "./components/styles/Global";
+import { Header } from "./components/styles/Header.styled";
+import { Footer } from "./components/styles/Footer.styled";
+import Quotes from "./components/Quotes";
+import {
+  ThemeContainer,
+  ThemeButton,
+} from "./components/styles/ThemeSwitching.styled";
+import { ThemeProvider } from "styled-components";
+import {
+  light,
+  dark,
+  blue,
+  green,
+  brown,
+  pink,
+} from "./components/styles/Theme.styled";
 
 function App() {
-  const {theme, themeLoaded, getFonts} = useTheme();
-  const [selectedTheme, setSelectedTheme] = useState(theme);
-  const [showDialog, setShowDialog] = useState(false);
-  const [newTheme, setNewTheme] = useState();
-  
- useEffect(() => {
-    WebFont.load({
-      google: {
-        families: getFonts()
-      }
-    });
-  });
+  const [selectedTheme, setSelectedTheme] = useState(light);
+  const [name, setname] = useState("");
+  const [header, setheader] = useState(light.colors.header);
+  const [background, setBackground] = useState(light.colors.background);
+  const [footer, setFooter] = useState(light.colors.footer);
+  const [text, setText] = useState(light.colors.text);
+
+  const HandleThemeChange = (theme) => {
+    setSelectedTheme(theme);
+    localStorage.setItem("current-theme", JSON.stringify(theme));
+  };
 
   useEffect(() => {
+    const currentTheme = JSON.parse(localStorage.getItem("current-theme"));
+    if (currentTheme) {
+      setSelectedTheme(currentTheme);
+    }
+  }, []);
+
+  const handleChange = () => {
+    const theme = {
+      name: name,
+      colors: {
+        header: header,
+        background: background,
+        footer: footer,
+        text: text,
+        quoteBgc: "hsl(60, 40%, 100%)",
+        quoteTitle: "hsl(0, 1%, 38%)",
+        quoteBody: "hsl(0, 1%, 38%);",
+        quoteBorder: "hsl(0, 0%, 87%)",
+        border: "hsl(0, 0%, 87%)",
+      },
+    };
     setSelectedTheme(theme);
-  }, [themeLoaded]);
-
-  const manageDialog = () => {
-    setShowDialog(!showDialog);
-  }
-
-  const createTheme = newTheme => {
-    console.log(newTheme);
-    setShowDialog(false);
-    setNewTheme(newTheme);
-  }
+    localStorage.setItem("current-theme", JSON.stringify(theme));
+  };
 
   return (
-    <>
-    {
-      themeLoaded && <ThemeProvider theme={ selectedTheme }>
-        <GlobalStyles/>
-        <Container style={{fontFamily: selectedTheme.font}}>
-          <h1>Theming System</h1>
+    <ThemeProvider theme={selectedTheme}>
+      <div className="App">
+        <GlobalStyles />
+        <Header>Game of Thrones Quotes</Header>
+        <ThemeContainer>
+          <span>Themes: </span>
+          <ThemeButton
+            className={`light ${selectedTheme === light ? "active" : ""}`}
+            onClick={() => HandleThemeChange(light)}
+          ></ThemeButton>
+          <ThemeButton
+            className={`dark ${selectedTheme === dark ? "active" : ""}`}
+            onClick={() => HandleThemeChange(dark)}
+          ></ThemeButton>
+          <ThemeButton
+            className={`blue ${selectedTheme === blue ? "active" : ""}`}
+            onClick={() => HandleThemeChange(blue)}
+          ></ThemeButton>
+          <ThemeButton
+            className={`green ${selectedTheme === green ? "active" : ""}`}
+            onClick={() => HandleThemeChange(green)}
+          ></ThemeButton>
+          <ThemeButton
+            className={`brown ${selectedTheme === brown ? "active" : ""}`}
+            onClick={() => HandleThemeChange(brown)}
+          ></ThemeButton>
+          <ThemeButton
+            className={`pink ${selectedTheme === pink ? "active" : ""}`}
+            onClick={() => HandleThemeChange(pink)}
+          ></ThemeButton>
+        </ThemeContainer>
+        <Quotes />
+        <div>
+          <label>Set Name</label>
+          <input
+            placeholder="Set name"
+            defaultValue={name}
+            onChange={(e) => setname(e.target.value)}
+          />
+          <br />
+          <label>Select Header</label>
+          <input
+            type="color"
+            defaultValue={header}
+            onChange={(e) => setheader(e.target.value)}
+          />
+          <br />
+          <label>Select background</label>
+          <input
+            type="color"
+            defaultValue={background}
+            onChange={(e) => setBackground(e.target.value)}
+          />
+          <br />
+          <label>Select footer</label>
+          <input
+            type="color"
+            defaultValue={footer}
+            onChange={(e) => setFooter(e.target.value)}
+          />
+          <br />
+          <label>Select text</label>
+          <input
+            type="color"
+            defaultValue={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <br />
+          <button onClick={handleChange}>Make</button>
+        </div>
+        <Footer>
           <p>
-            Hey, There! It's great when the control is with you. The theming system
-            helps you in building a theme of your choice and apply it to test live. Why
-            wait? Just give it a try.
+            Made with love by <a href="http://bio.link/timonwa">Timonwa</a>
           </p>
-          <button className="btn" onClick={ manageDialog }>Create a Theme</button>
-          <Dialog 
-            header="Create a Theme"
-            body={ <CreateThemeContent create={ createTheme }/> }
-            open={ showDialog } 
-            callback = { manageDialog }/>
-          <ThemeSelector setter={ setSelectedTheme } newTheme={ newTheme }/>
-        </Container>
-      </ThemeProvider>
-    }
-    </>
+        </Footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
